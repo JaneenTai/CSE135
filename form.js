e8f8c34fb6223c602950614f22a5be6d4c0b9bd4
Janeen Taitingfong <jatai@Janeens-MacBook-Air.local>
2026-08-17 17:00:15 -0700

    testing echo form

---
+++ form.js
@@ -0,0 +1,18 @@
+const form = document.getElementById('echo-form');
+
+form.addEventListener('submit', async function (event) {
+    event.preventDefault(); // stop the normal HTML form submission
+
+    const method = document.getElementById('method').value;
+    const queryString = document.getElementById('query').value;
+
+    if(method == 'GET'){
+        fetch('/cgi-bin/mypage.pl')
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById('container').innerHTML = html;
+        })
+        .catch(error => console.error('Error fetching page:', error));
+    }
+    
+});
