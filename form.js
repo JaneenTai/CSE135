ea427c93de6904c2de5ccf6ef568f14f9b239d4e
Janeen Taitingfong <jatai@Janeens-MacBook-Air.local>
2026-08-17 17:07:04 -0700

    testing small things

--- form.js
+++ form.js
@@ -5,14 +5,9 @@ form.addEventListener('submit', async function (event) {
 
     const method = document.getElementById('method').value;
     const queryString = document.getElementById('query').value;
+    const lang = document.getElementById('language').value;
 
     if(method == 'GET'){
-        fetch('cgi-bin/perl-general-echo.pl')
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('container').innerHTML = html;
-        })
-        .catch(error => console.error('Error fetching page:', error));
+        console.log("if statement works", lang);
     }
-    
 });
